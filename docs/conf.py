f24f1636ddbddbb855f3d41d064dfa873a304db8
James <gobot1234yt@gmail.com>
2021-10-25 23:20:53 +0100

    Maybe this helps?

--- docs/conf.py
+++ docs/conf.py
@@ -4,11 +4,16 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import sys
+import os.path
+
+import betterproto
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-
-import betterproto
+sys.path.insert(0, os.path.abspath(".."))
+sys.path.append(os.path.abspath("extensions"))
 
 
 # -- Project information -----------------------------------------------------
